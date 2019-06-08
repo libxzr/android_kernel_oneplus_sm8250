461a214f590125aca12ab1fa6ecf22b7dab096c6
Tyler Nijmeh <tylernij@gmail.com>
2019-06-08 16:54:31 -0700

    block: Do not collect I/O statistics
    This adds a great deal of latency to block requests due to disabling
    preemption temporarily during statistics collection.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -707,12 +707,10 @@ struct request_queue {
 #define QUEUE_FLAG_QUIESCED    28	/* queue has been quiesced */
 #define QUEUE_FLAG_INLINECRYPT 29	/* inline encryption support */
 
-#define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_IO_STAT) |		\
-				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
+#define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_SAME_COMP)	|	\
 				 (1 << QUEUE_FLAG_ADD_RANDOM))
 
-#define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_IO_STAT) |		\
-				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
+#define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_SAME_COMP)	|	\
 				 (1 << QUEUE_FLAG_POLL))
 
 void blk_queue_flag_set(unsigned int flag, struct request_queue *q);
