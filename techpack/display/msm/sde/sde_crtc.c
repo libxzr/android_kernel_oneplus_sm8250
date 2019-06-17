0307d0c85f12be4275aaa056a12e66e5c30fdfe9
Vache <vachounet@live.fr>
2019-06-17 14:53:29 -0500

    gpu: drm: Force fp pressed if fp mode on
    Change-Id: Ide2c2b4fafb00fe5ae4063b852d2d19be252f63d

--- techpack/display/msm/sde/sde_crtc.c
+++ techpack/display/msm/sde/sde_crtc.c
@@ -4977,6 +4977,15 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
 	else
 		display->panel->dim_status = false;
 
+	if (fp_mode == 1) {
+		cstate->fingerprint_pressed = true;
+		return 0;
+	} else {
+		cstate->fingerprint_pressed = false;
+		cstate->fingerprint_dim_layer = NULL;
+		return 0;
+	}
+
 	if(aod_index <0){
 		oneplus_aod_hid = 0;
 		aod_layer_hide = 0;
