a2693b6a08366a356f58723029b8781f6e153c50
Yaroslav Furman <yaro330@gmail.com>
2019-06-20 16:51:50 +0300

    cpu_input_boost: add support for boost freqs lower than min freq
    Adds ability to disable boosting of certain clusters by
    setting boost freq to 0 for it. Otherwise freq would
    drop to cpuinfo.min, which isn't desirable.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -97,11 +97,11 @@ static unsigned int get_input_boost_freq(struct cpufreq_policy *policy)
 	unsigned int freq;
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
-		freq = input_boost_freq_little;
+		freq = max(input_boost_freq_little, cpu_freq_min_little);
 	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
-		freq = input_boost_freq_big;
+		freq = max(input_boost_freq_big, cpu_freq_min_big);
 	else
-		freq = input_boost_freq_prime;
+		freq = max(input_boost_freq_prime, cpu_freq_min_prime);
 	return min(freq, policy->max);
 }
 
