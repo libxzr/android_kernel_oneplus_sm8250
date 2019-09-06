204e9b1025e705827fc40a9aa641c0d2a8d497fc
Peter Cai <peter@typeblog.net>
2019-09-06 15:32:11 +0800

    drm: msm: default to FOD dimming when calculating alpha
    * Since commit ac878e6b, DC dimming has been introduced, which shares
    some logic with FOD. The original function to calculate alpha to dim the
    screen to the same brightness before enabling HBM has been modified to
    also work with DC dimming. It distinguishes the two cases (FOD and DC)
    with the flag `dim_status`.
    
    * However, in our user-space implementation of FOD, the `dim_status`
    flag is only set AFTER the FOD icon has been pressed by the user, unlike
    in official ROM where they enable dimming whenever the FOD icon shows
    up which breaks the image quality terribly. Our different order caused
    the userspace code to read the alpha value before the flag is actually
    set, and thus being incorrect, causing severe blinking when using FOD.
    
    * Fix this by defaulting to using the flag `op_dimlayer_bl_enable` for
    distinguishing between FOD and DC, so as long as DC dimming is not
    enabled (which is always the case because we haven't even implemented DC
    dimming in our userspace), the value read by userspace is always the one
    for FOD. If we were ever to implement DC dimming, we should probably
    separate these two interfaces to two sysfs nodes instead of sharing a
    common one to avoid confusion.
    
    Change-Id: I1aa2bdad330a6b555f7025e6fff3fa1684052726

--- techpack/display/msm/sde/sde_crtc.c
+++ techpack/display/msm/sde/sde_crtc.c
@@ -2767,6 +2767,7 @@ int bl_to_alpha_dc(int brightness)
 	return alpha;
 }
 
+extern int op_dimlayer_bl_enable;
 int oneplus_get_panel_brightness_to_alpha(void)
 {
 	struct dsi_display *display = get_main_display();
@@ -2775,7 +2776,7 @@ int oneplus_get_panel_brightness_to_alpha(void)
 		return 0;
 	if (oneplus_panel_alpha)
 		return oneplus_panel_alpha;
-    if (display->panel->dim_status)
+    if (!op_dimlayer_bl_enable || display->panel->dim_status)
 		return brightness_to_alpha(display->panel->hbm_backlight);
     else
 	return bl_to_alpha_dc(display->panel->hbm_backlight);
