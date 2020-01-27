784432a0e4338205a1b84622bfcf264f9434c044
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-01-28 00:17:02 +0530

    devfreq: update device name as per actual node being used
    We are currently boosting just LLCBW DDR and NOT CPUBW, this should've been
    updated when it was adapted for 4.14.

--- drivers/devfreq/devfreq_devbw.c
+++ drivers/devfreq/devfreq_devbw.c
@@ -181,7 +181,7 @@ int devfreq_add_devbw(struct device *dev)
 	}
 	
 	if (!strcmp(dev_name(dev), "soc:qcom,cpu-llcc-ddr-bw"))
-		devfreq_register_boost_device(DEVFREQ_MSM_CPUBW, d->df);
+		devfreq_register_boost_device(DEVFREQ_MSM_LLCCBW_DDR, d->df);
 
 	return 0;
 }
