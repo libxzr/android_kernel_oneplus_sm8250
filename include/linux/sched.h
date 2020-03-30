6ea26aa078b740ef343a869a67480e2bc3ac2c2b
LibXZR <xzr467706992@163.com>
2020-03-30 22:45:05 +0800

    sched/tune: Offload dynamic stune boost onto workers
    * Whatever, we shouldn't do hard works on main thread, as it is called in frame buffer
    
    Signed-off-by: LibXZR <xzr467706992@163.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2224,8 +2224,8 @@ static inline void set_wake_up_idle(bool enabled)
 }
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
-int do_stune_boost(char *st_name, int boost);
-int reset_stune_boost(char *st_name);
+void do_stune_boost(char *st_name, int boost);
+void reset_stune_boost(char *st_name);
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 #endif
