ab4f65c9a6559845a4ce0f421bae54e1cc4532f1
LibXZR <xzr467706992@163.com>
2020-07-31 22:02:33 +0800

    drivers: rtc: Import oneplus changes
    * From OnePlus 8 Oxygen OS 10.5.11.IN21AA and OnePlus 8 Pro Oxygen OS 10.5.12.IN11AA

--- drivers/rtc/rtc-pm8xxx.c
+++ drivers/rtc/rtc-pm8xxx.c
@@ -13,6 +13,8 @@
 #include <linux/slab.h>
 #include <linux/spinlock.h>
 
+static  bool print_cureent_time;
+
 /* RTC Register offsets from RTC CTRL REG */
 #define PM8XXX_ALARM_CTRL_OFFSET	0x01
 #define PM8XXX_RTC_WRITE_OFFSET		0x02
@@ -182,6 +184,7 @@ static int pm8xxx_rtc_read_time(struct device *dev, struct rtc_time *tm)
 	unsigned int reg;
 	struct pm8xxx_rtc *rtc_dd = dev_get_drvdata(dev);
 	const struct pm8xxx_rtc_regs *regs = rtc_dd->regs;
+	static int is_first_time = 1;
 
 	rc = regmap_bulk_read(rtc_dd->regmap, regs->read, value, sizeof(value));
 	if (rc) {
@@ -213,10 +216,21 @@ static int pm8xxx_rtc_read_time(struct device *dev, struct rtc_time *tm)
 
 	rtc_time_to_tm(secs, tm);
 
+	if (is_first_time) {
+		/* print timestamp for debug */
+		dev_err(dev, "rtc timestamp = %lu\n", secs);
+		is_first_time = 0;
+	}
+
 	dev_dbg(dev, "secs = %lu, h:m:s == %d:%d:%d, d/m/y = %d/%d/%d\n",
 		secs, tm->tm_hour, tm->tm_min, tm->tm_sec,
 		tm->tm_mday, tm->tm_mon, tm->tm_year);
-
+	if (print_cureent_time) {
+		print_cureent_time = false;
+		dev_info(dev, "current time:secs = %lu, h:m:s == %d:%d:%d, d/m/y = %d/%d/%d\n",
+			secs, tm->tm_hour, tm->tm_min, tm->tm_sec,
+			tm->tm_mday, tm->tm_mon, tm->tm_year);
+	}
 	return 0;
 }
 
@@ -228,13 +242,28 @@ static int pm8xxx_rtc_set_alarm(struct device *dev, struct rtc_wkalrm *alarm)
 	unsigned long secs, irq_flags;
 	struct pm8xxx_rtc *rtc_dd = dev_get_drvdata(dev);
 	const struct pm8xxx_rtc_regs *regs = rtc_dd->regs;
+	static u8 pre_value_0, pre_value_1, pre_value_2, pre_value_3;
+	static int alarm_en_pre;
 
 	rtc_tm_to_time(&alarm->time, &secs);
-
 	for (i = 0; i < NUM_8_BIT_RTC_REGS; i++) {
 		value[i] = secs & 0xFF;
 		secs >>= 8;
 	}
+	if (value[0] != pre_value_0 || value[1] != pre_value_1
+					|| value[2] != pre_value_2 || value[3] != pre_value_3) {
+		dev_info(dev, "val[0] = 0x%x, val[1] = 0x%x, val[2] = 0x%x, val[3] = 0x%x\n",
+					value[0], value[1], value[2], value[3]);
+		dev_info(dev, "Alarm Set for h:r:s=%d:%d:%d, d/m/y=%d/%d/%d\n",
+					alarm->time.tm_hour, alarm->time.tm_min,
+					alarm->time.tm_sec, alarm->time.tm_mday,
+					alarm->time.tm_mon, alarm->time.tm_year);
+		print_cureent_time = true;
+	}
+	pre_value_0 = value[0];
+	pre_value_1 = value[1];
+	pre_value_2 = value[2];
+	pre_value_3 = value[3];
 
 	spin_lock_irqsave(&rtc_dd->ctrl_reg_lock, irq_flags);
 
@@ -259,6 +288,9 @@ static int pm8xxx_rtc_set_alarm(struct device *dev, struct rtc_wkalrm *alarm)
 		dev_err(dev, "Write to RTC alarm control register failed\n");
 		goto rtc_rw_fail;
 	}
+	if (alarm_en_pre != alarm->enabled)
+		dev_info(dev, "alarm->enabled:%d\n", alarm->enabled);
+	alarm_en_pre = alarm->enabled;
 
 	dev_dbg(dev, "Alarm Set for h:r:s=%d:%d:%d, d/m/y=%d/%d/%d\n",
 		alarm->time.tm_hour, alarm->time.tm_min,
@@ -335,6 +367,12 @@ static int pm8xxx_rtc_alarm_irq_enable(struct device *dev, unsigned int enable)
 	if (rc) {
 		dev_err(dev, "Write to RTC control register failed\n");
 		goto rtc_rw_fail;
+	}
+	/* Clear Alarm register */
+	if (!enable) {
+		rc = regmap_bulk_write(rtc_dd->regmap, regs->alarm_rw, value, NUM_8_BIT_RTC_REGS);
+		if (rc)
+			dev_err(dev, "Write to RTC ALARM register failed\n");
 	}
 
 	/* Clear Alarm register */
