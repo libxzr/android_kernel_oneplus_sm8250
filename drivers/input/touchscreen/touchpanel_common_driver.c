c1da150add819eb3285b60ca34c56c0e059ee02b
LibXZR <xzr467706992@163.com>
2020-08-11 17:36:24 +0800

    touchpanel_common_driver: Disable logging
    * Also cleanup an unused variable

--- drivers/input/touchscreen/touchpanel_common_driver.c
+++ drivers/input/touchscreen/touchpanel_common_driver.c
@@ -37,26 +37,12 @@
 /*******Part0:LOG TAG Declear************************/
 #define TPD_PRINT_POINT_NUM 150
 #define TPD_DEVICE "touchpanel"
-#define TPD_INFO(a, arg...)  pr_err("[TP]"TPD_DEVICE ": " a, ##arg)
-#define TPD_DEBUG(a, arg...)\
-	do{\
-		if (LEVEL_DEBUG == tp_debug)\
-		pr_err("[TP]"TPD_DEVICE ": " a, ##arg);\
-	}while(0)
+#define TPD_INFO(a, arg...)
+#define TPD_DEBUG(a, arg...)
 
-#define TPD_DETAIL(a, arg...)\
-	do{\
-		if (LEVEL_BASIC != tp_debug)\
-		pr_err("[TP]"TPD_DEVICE ": " a, ##arg);\
-	}while(0)
+#define TPD_DETAIL(a, arg...)
 
-#define TPD_SPECIFIC_PRINT(count, a, arg...)\
-	do{\
-		if (count++ == TPD_PRINT_POINT_NUM || LEVEL_DEBUG == tp_debug) {\
-			TPD_INFO(TPD_DEVICE ": " a, ##arg);\
-			count = 0;\
-		}\
-	}while(0)
+#define TPD_SPECIFIC_PRINT(count, a, arg...)
 
 /*******Part1:Global variables Area********************/
 unsigned int tp_debug = 0;
@@ -230,7 +216,6 @@ void operate_mode_switch(struct touchpanel_data *ts)
 static void tp_touch_down(struct touchpanel_data *ts, struct point_info points, int touch_report_num, int id)
 {
 	static int last_width_major;
-	static int point_num = 0;
 
 	if (ts->input_dev == NULL)
 		return;
