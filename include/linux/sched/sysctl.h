007eebb01ffbf2e1c4825d69d82b2a0f8d904470
LibXZR <xzr467706992@163.com>
2020-08-11 22:31:58 +0800

    schedhorizon: Introduce schedhorizon cpufreq governor
    * This is a modified version of schedutil, introducing two new tunables: "efficient_freq" and "up_delay".
    * Only raise cpufreq to the non-efficient one (higher than effcient frequencies) if the governor keeps requiring non-efficient frequencies for more than up_delay time.
    * Override the new frequencies with the efficient one if the consecutive request time doesn't reach up_delay.
    * The two tunables support multiple args, e.g. you can set "1248000 1401600" for "efficient_freq" and set "50 60" for "up_delay", which means it would wait 50ms before raising the frequency to 1248mhz and wait for 60ms before raising the frequency to 1401mhz.
    
    Signed-off-by: LibXZR <xzr467706992@163.com>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -162,7 +162,7 @@ extern char sched_lib_name[LIB_PATH_LENGTH];
 extern unsigned int sched_lib_mask_force;
 extern bool is_sched_lib_based_app(pid_t pid);
 
-#if defined(CONFIG_ENERGY_MODEL) && defined(CONFIG_CPU_FREQ_GOV_SCHEDUTIL)
+#if defined(CONFIG_ENERGY_MODEL) && ((defined(CONFIG_CPU_FREQ_GOV_SCHEDUTIL) || defined(CONFIG_CPU_FREQ_GOV_SCHEDHORIZON)))
 extern unsigned int sysctl_sched_energy_aware;
 extern int sched_energy_aware_handler(struct ctl_table *table, int write,
 				 void __user *buffer, size_t *lenp,
