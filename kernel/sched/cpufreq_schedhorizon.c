d5c3765b3ee82f88704633f1c7598c6f39e73322
LibXZR <xzr467706992@163.com>
2020-08-12 10:16:00 +0800

    schedhorizon: Tweak default settings

--- kernel/sched/cpufreq_schedhorizon.c
+++ kernel/sched/cpufreq_schedhorizon.c
@@ -20,11 +20,11 @@
 static unsigned int default_efficient_freq_lp[] = {0};
 static u64 default_up_delay_lp[] = {0};
 
-static unsigned int default_efficient_freq_hp[] = {0};
-static u64 default_up_delay_hp[] = {0};
+static unsigned int default_efficient_freq_hp[] = {1286400, 1670400, 2054400};
+static u64 default_up_delay_hp[] = {10 * NSEC_PER_MSEC, 20 * NSEC_PER_MSEC, 470 * NSEC_PER_MSEC};
 
-static unsigned int default_efficient_freq_pr[] = {0};
-static u64 default_up_delay_pr[] = {0};
+static unsigned int default_efficient_freq_pr[] = {1632000, 2073600, 2457600};
+static u64 default_up_delay_pr[] = {10 * NSEC_PER_MSEC, 20 * NSEC_PER_MSEC, 470 * NSEC_PER_MSEC};
 
 #define DEFAULT_RTG_BOOST_FREQ_LP 1000000
 #define DEFAULT_RTG_BOOST_FREQ_HP 0
@@ -38,9 +38,9 @@ static u64 default_up_delay_pr[] = {0};
 #define DEFAULT_HISPEED_FREQ_HP 0
 #define DEFAULT_HISPEED_FREQ_PR 0
 
-#define DEFAULT_PL_LP 0
-#define DEFAULT_PL_HP 0
-#define DEFAULT_PL_PR 0
+#define DEFAULT_PL_LP 1
+#define DEFAULT_PL_HP 1
+#define DEFAULT_PL_PR 1
 
 struct sugov_tunables {
 	struct gov_attr_set	attr_set;
