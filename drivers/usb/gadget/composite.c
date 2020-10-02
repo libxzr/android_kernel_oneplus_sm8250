d812d87fc87b4f60632d53be20b759a809d515eb
LibXZR <xzr467706992@163.com>
2020-10-02 15:03:48 +0800

    drivers: usb: Import oneplus changes
    * From OnePlus 9R Oxygen OS 11.2.1.1.LE28DA
    
    Signed-off-by: LibXZR <xzr467706992@163.com>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -1784,12 +1784,16 @@ composite_setup(struct usb_gadget *gadget, const struct usb_ctrlrequest *ctrl)
 					cdev->desc.bcdUSB = cpu_to_le16(0x0320);
 					cdev->desc.bMaxPacketSize0 = 9;
 				} else {
-					cdev->desc.bcdUSB = cpu_to_le16(0x0210);
+				//cdev->desc.bcdUSB = cpu_to_le16(0x0210);
+					cdev->desc.bcdUSB = cpu_to_le16(0x0200);
 				}
 			} else {
+				/* add to fix carlife issue */
+				/*
 				if (gadget->lpm_capable)
 					cdev->desc.bcdUSB = cpu_to_le16(0x0201);
 				else
+				*/
 					cdev->desc.bcdUSB = cpu_to_le16(0x0200);
 			}
 
@@ -2470,7 +2474,7 @@ void composite_suspend(struct usb_gadget *gadget)
 	spin_unlock_irqrestore(&cdev->lock, flags);
 
 	usb_gadget_set_selfpowered(gadget);
-	usb_gadget_vbus_draw(gadget, 2);
+	//usb_gadget_vbus_draw(gadget, 2);
 }
 
 void composite_resume(struct usb_gadget *gadget)
