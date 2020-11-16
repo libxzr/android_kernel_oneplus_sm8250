d76e4b3afc30a0818510252abfc15537d7ec964d
LibXZR <xzr467706992@163.com>
2020-11-16 21:23:47 +0800

    touchpanel_common_driver: Restore game mode status after switching touchpanel sample rate
    * Applying a new sample rate could cause game mode bening disabled even when "game_switch" is still enabling.

--- drivers/input/touchscreen/touchpanel_common_driver.c
+++ drivers/input/touchscreen/touchpanel_common_driver.c
@@ -5551,6 +5551,8 @@ static int tfb_notifier_callback(struct notifier_block *self, unsigned long even
 				mutex_lock(&ts->mutex);
 				if (!ts->is_suspended && (ts->suspend_state == TP_SPEEDUP_RESUME_COMPLETE)) {
 					ts->ts_ops->mode_switch(ts->chip_data, MODE_REFRESH_SWITCH, 0);
+					if (ts->noise_level)
+						ts->ts_ops->mode_switch(ts->chip_data, MODE_GAME, true);
 				}
 				mutex_unlock(&ts->mutex);
 			}
@@ -5559,6 +5561,8 @@ static int tfb_notifier_callback(struct notifier_block *self, unsigned long even
 				mutex_lock(&ts->mutex);
 				if (!ts->is_suspended && (ts->suspend_state == TP_SPEEDUP_RESUME_COMPLETE)) {
 					ts->ts_ops->mode_switch(ts->chip_data, MODE_REFRESH_SWITCH, 1);
+					if (ts->noise_level)
+						ts->ts_ops->mode_switch(ts->chip_data, MODE_GAME, true);
 				}
 				mutex_unlock(&ts->mutex);
 			}
@@ -5567,6 +5571,8 @@ static int tfb_notifier_callback(struct notifier_block *self, unsigned long even
 				mutex_lock(&ts->mutex);
 				if (!ts->is_suspended && (ts->suspend_state == TP_SPEEDUP_RESUME_COMPLETE)) {
 					ts->ts_ops->mode_switch(ts->chip_data, MODE_REFRESH_SWITCH, 1);
+					if (ts->noise_level)
+						ts->ts_ops->mode_switch(ts->chip_data, MODE_GAME, true);
 				}
 				mutex_unlock(&ts->mutex);
 			}
