fc3624d843ea94795fb523efb8dc1095569d65a0
LibXZR <xzr467706992@163.com>
2020-12-21 14:44:24 +0800

    touchscreen: s6sy761: Relax CPU latency requirement to save power
    * This allows small cpus to enter C4 state, saving power slightly without hurting performance much

--- drivers/input/touchscreen/samsung/s6sy761/sec_drivers_s6sy761.c
+++ drivers/input/touchscreen/samsung/s6sy761/sec_drivers_s6sy761.c
@@ -27,7 +27,7 @@
 static struct dma_buf_s6sy761 *dma_buffer;
 extern int tp_register_times;
 extern struct touchpanel_data *g_tp;
-#define PM_QOS_VALUE_TP 200
+#define PM_QOS_VALUE_TP 532
 struct pm_qos_request pm_qos_req_stp;
 
 /****************** Start of Log Tag Declear and level define*******************************/
