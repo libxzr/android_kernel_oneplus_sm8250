988198178945ca0a21eea76f7118f8fd33472f4e
LibXZR <xzr467706992@163.com>
2020-12-24 22:26:58 +0800

    cpuset: Introduce dynamic cpuset for display group
    * Sometimes we have to kick display tasks onto high performance cluster in order to reduce frame drop, for example, when switching between apps in multitask ui.
    
    Signed-off-by: LibXZR <xzr467706992@163.com>

--- include/linux/cpuset.h
+++ include/linux/cpuset.h
@@ -160,6 +160,9 @@ static inline void set_mems_allowed(nodemask_t nodemask)
 	task_unlock(current);
 }
 
+void do_hp_cpuset(void);
+void do_lp_cpuset(void);
+
 #else /* !CONFIG_CPUSETS */
 
 static inline bool cpusets_enabled(void) { return false; }
