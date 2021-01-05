ce2534a47e3ec53d878d3758df424983f29106a0
alk3pInjection <webmaster@raspii.tech>
2021-01-06 01:30:25 +0800

    mm: Hard-code maximum swappiness to 200
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Signed-off-by: LibXZR <xzr467706992@163.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -136,6 +136,7 @@ static unsigned long one_ul = 1;
 static unsigned long long_max = LONG_MAX;
 static int one_hundred = 100;
 static int one_thousand = 1000;
+static int max_swappiness = 200;
 #ifdef CONFIG_PRINTK
 static int ten_thousand = 10000;
 #endif
@@ -1756,7 +1757,7 @@ static struct ctl_table vm_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= &zero,
-		.extra2		= &one_hundred,
+		.extra2		= &max_swappiness,
 	},
 	{
 		.procname       = "want_old_faultaround_pte",
