774a11bf06485d5c8dc07c7bd0416ecafc29bfa6
Adam W. Willis <return.of.octobot@gmail.com>
2021-01-15 18:41:22 -0500

    qcacmn: Properly guard WMI debugging subsequent to LA.UM.9.12.r1-10000
    Redefinition of wmi_debug when !WMI_INTERFACE_EVENT_LOGGING in 0ctobot/neutrino_kernel_oneplus_sm8250@3c89110
    results in the following compilation warning subsequent to 0ctobot/neutrino_kernel_oneplus_sm8250@7035a36:
    
    ../drivers/staging/qcacld-3.0/../qca-wifi-host-cmn/wmi/src/wmi_unified_sta_tlv.c:1364:20: warning: unused function 'wmi_sar2_result_string' [-Wunused-function]
    static const char *wmi_sar2_result_string(uint32_t result)
                       ^
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: LibXZR <xzr467706992@163.com>

--- drivers/staging/qca-wifi-host-cmn/wmi/src/wmi_unified_sta_tlv.c
+++ drivers/staging/qca-wifi-host-cmn/wmi/src/wmi_unified_sta_tlv.c
@@ -1352,6 +1352,7 @@ static QDF_STATUS get_sar_limit_cmd_tlv(wmi_unified_t wmi_handle)
 	return status;
 }
 
+#ifdef WMI_INTERFACE_EVENT_LOGGING
 /**
  * wmi_sar2_result_string() - return string conversion of sar2 result
  * @result: sar2 result value
@@ -1373,6 +1374,7 @@ static const char *wmi_sar2_result_string(uint32_t result)
 		return "Unknown response";
 	}
 }
+#endif /*WMI_INTERFACE_EVENT_LOGGING */
 
 /**
  * extract_sar2_result_event_tlv() -  process sar response event from FW.
