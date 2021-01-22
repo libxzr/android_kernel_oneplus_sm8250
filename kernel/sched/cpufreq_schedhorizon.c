5c5cff1a77071996700de8c532d73e0368bc6fc4
LibXZR <xzr467706992@163.com>
2021-01-22 20:50:07 +0800

    schedhorizon: Don't limit frequency if is gaming
    Signed-off-by: LibXZR <xzr467706992@163.com>

--- kernel/sched/cpufreq_schedhorizon.c
+++ kernel/sched/cpufreq_schedhorizon.c
@@ -188,8 +188,13 @@ static inline int match_nearest_efficient_step(int freq, int maxstep, int *freq_
 	return i;
 }
 
+extern bool is_gaming __read_mostly;
+
 static inline void do_freq_limit(struct sugov_policy *sg_policy, unsigned int *freq, u64 time)
 {
+	if (is_gaming)
+		return;
+
 	if (*freq > sg_policy->tunables->efficient_freq[sg_policy->tunables->current_step] && !sg_policy->first_hp_request_time) {
 		/* First request */
 		*freq = sg_policy->tunables->efficient_freq[sg_policy->tunables->current_step];
