4eada8389f50c829c61aa1061ef6d4f60422e535
LibXZR <xzr467706992@163.com>
2021-02-28 22:04:01 +0800

    sched/walt: Never allow userspace affect load scale
    * It makes no sense

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -3674,6 +3674,8 @@ int walt_proc_user_hint_handler(struct ctl_table *table,
 	unsigned int old_value;
 	static DEFINE_MUTEX(mutex);
 
+	return 0;
+
 	mutex_lock(&mutex);
 
 	sched_user_hint_reset_time = jiffies + HZ;
