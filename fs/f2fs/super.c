28010c29cca68c45d72ce2f7ffbdae673d4ebcdc
LibXZR <xzr467706992@163.com>
2021-07-20 23:07:56 +0800

    f2fs: Move to late_initcall
    * We have to wait panel init done, or the drm notifier won't be successfully registered.
    
    Signed-off-by: LibXZR <xzr467706992@163.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -4031,10 +4031,4 @@ static void __exit exit_f2fs_fs(void)
 	f2fs_destroy_trace_ios();
 }
 
-module_init(init_f2fs_fs)
-module_exit(exit_f2fs_fs)
-
-MODULE_AUTHOR("Samsung Electronics's Praesto Team");
-MODULE_DESCRIPTION("Flash Friendly File System");
-MODULE_LICENSE("GPL");
-
+late_initcall(init_f2fs_fs);
