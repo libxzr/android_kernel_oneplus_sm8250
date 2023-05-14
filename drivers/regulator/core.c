cc9c5b5555da358da0613a7796bf98f0d28aaae6
LibXZR <i@xzr.moe>
2023-05-14 11:14:49 +0800

    treewide: Clean up oplus rpmh powerinfo changes
    We don't need this in a release build.
    
    Signed-off-by: LibXZR <i@xzr.moe>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -5266,16 +5266,6 @@ void regulator_debug_print_enabled(void)
 }
 EXPORT_SYMBOL(regulator_debug_print_enabled);
 
-#ifdef OPLUS_FEATURE_POWERINFO_RPMH
-void oplus_show_regulator_list(void)
-{
-	pr_info("oplus_show_regulator_list:Enabled regulators\n");
-	class_for_each_device(&regulator_class, NULL, NULL,
-			     _regulator_debug_print_enabled);
-}
-EXPORT_SYMBOL(oplus_show_regulator_list);
-#endif
-
 static int __init regulator_init(void)
 {
 	int ret;
